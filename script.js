b1e721533ea14d6dc6acc3a01116d0859d630fb0
hadiascripte <hadiascripte@gmail.com>
2025-04-15 14:14:39 +0200

    ok

--- script.js
+++ script.js
@@ -10,66 +10,35 @@
 //     alert("Vous êtes déconnecté.");
 // });
 
-
-let input = document.querySelector("#prenom");
-const envoie = document.querySelector("#btnEnvoi");
+// let input = document.querySelector("#prenom");
+// const envoie = document.querySelector("#btnEnvoi");
 // const connexion = document.querySelector("#btnConnexion");
 // const deconnexion = document.querySelector("#btnDeconnexion");
 
-const divConnexion = document.querySelector("#divConnexion");
-const divbtn = document.querySelectorAll("#divConnexion input");
+// const divConnexion = document.querySelector("#divConnexion");
+// const divbtn = document.querySelectorAll("#divConnexion input");
 
-console.log(input, envoie);
-divConnexion.style.display = "none";
+// console.log(input, envoie);
+// divConnexion.style.display = "none";
 
-envoie.addEventListener("click", (userValue) => {
-    const affichage = document.querySelector("#affichage");
-    userValue = input.value;
-    document.cookie = `nom=${userValue}; max-age=36000; path=/`;
-    let affichageValue = document.cookie.split(";")[0];
-    let prenom = affichageValue.split("=")[1];
+// envoie.addEventListener("click", (userValue) => {
+//     const affichage = document.querySelector("#affichage");
+//     userValue = input.value;
+//     document.cookie = `nom=${userValue}; max-age=36000; path=/`;
+//     let affichageValue = document.cookie.split(";")[0];
+//     let prenom = affichageValue.split("=")[1];
 
-    divConnexion.style.display = "block";
-    affichage.innerHTML = `Bonjour <strong>${prenom} </strong> vous pouvez vous connecter !`;
+//     divConnexion.style.display = "block";
+//     affichage.innerHTML = `Bonjour <strong>${prenom} </strong> vous pouvez vous connecter !`;
 
-    console.log(userValue);
-    console.log(affichageValue);
-    console.log(prenom);
-});
-
-
-
-
-//Bouton de connexion
-
-// const btnConnexion = document.querySelector("#btnConnexion");console.log(btnConnexion);
-
-
-// btnConnexion.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     const inputPrenom = domucment.querySelector("#prenom").value;
-// console.log(inputPrenom);
-
-//     if (cookieData[inputPrenom]) {
-//         sessionStorage.setItem("name", inputPrenom);
-//         document.body.classList = cookieData[inputPrenom];
-//     }
+//     console.log(userValue);
+//     console.log(affichageValue);
+//     console.log(prenom);
 // });
 
-// Bouton déconnexion
+let input = document.querySelector("#prenom");
+const envoie = document.querySelector("#btnEnvoi");
 
-
-
-
-
-
-
-
-
-
-
-
-/*
 document.cookie = "raouf=light;max-age=36000;path=/";
 document.cookie = "tom=dark;max-age=36000;path=/";
 document.cookie = "nancy=light;max-age=36000;path=/";
@@ -94,6 +63,7 @@ for (let i = 0; i < cookies.length; i++) {
     console.log(value);
     console.log(key);
 }
+
 sessionStorage.setItem("name", "nancy");
 const sessionName = sessionStorage.getItem("name");
 if (sessionName && cookieData[sessionName]) {
@@ -101,5 +71,63 @@ if (sessionName && cookieData[sessionName]) {
     document.body.classList = cookieData[sessionName];
 }
 
-*/
+// Si déjà connecté et thème existant, appliquer le thème
+console.log("keyData", cookieData[sessionName]);
+// SI (J'ai une session  ET QUE le nom dans la session correspond à une key de cookieData){
+if (sessionName && cookieData[sessionName]) {
+    console.log(cookieData[sessionName]);
 
+    //on ajoute le thème au body
+    document.body.classList = cookieData[sessionName];
+}
+
+// Formulaire de connexion
+
+document.querySelector("#btnConnexion").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const prenom = document.querySelector("#btnConnexion").value;
+    // Vérifier si le cookie existe pour ce nom
+
+    if (cookieData[prenom]) {
+        console.log("cookieData[name] : ", cookieData[prenom]);
+
+        sessionStorage.setItem("name", prenom);
+        document.body.classList = cookieData[prenom]; // applique le thème
+        alert("Connexion reussie :), thème chargé !");
+    } else {
+        alert("Nom inconnu :( Veuillez vous enregistrer");
+    }
+});
+
+// Formulaire d'enregistrement
+document.querySelector("#btnConnexion").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const prenom = document.querySelector("#btnConnexion").value;
+    //la checkbox
+    const darkmode = document.querySelector("#register-darkmode").checked;
+    const theme = darkmode ? "dark" : "light";
+    // Créer cookie personnalisé : clé = nom, valeur = thème
+    let expiration = 3 * 24 * 3600;
+    document.cookie = `${prenom}=${theme}; max-age=${expiration}; path=/`;
+    // Stocker dans session
+    sessionStorage.setItem("name", prenom);
+    // Appliquer thème
+    document.body.classList.add(theme);
+    alert("enregistrement réussi +!!");
+});
+
+// // Bouton de connexion
+
+// const btnConnexion = document.querySelector("#btnConnexion");
+// console.log(btnConnexion);
+
+// btnConnexion.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     let inputPrenom = domucment.querySelector("#prenom").value;
+
+//     if (cookieData[inputPrenom]) {
+//         sessionStorage.setItem("name", inputPrenom);
+//         console.log(inputPrenom);
+//         document.body.classList = cookieData[inputPrenom];
+//     }
+// });
